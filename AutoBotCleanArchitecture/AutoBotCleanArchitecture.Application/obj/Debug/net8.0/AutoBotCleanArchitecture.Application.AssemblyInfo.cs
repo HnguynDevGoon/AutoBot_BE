a7dc780aa00a5eeacdc20b3792d969663591c37c
HnguynDevGoon <hnguyndevgoon@gmail.com>
2025-11-15 11:04:09 +0700

    Fix LogHistory

--- AutoBotCleanArchitecture/AutoBotCleanArchitecture.Application/obj/Debug/net8.0/AutoBotCleanArchitecture.Application.AssemblyInfo.cs
+++ AutoBotCleanArchitecture/AutoBotCleanArchitecture.Application/obj/Debug/net8.0/AutoBotCleanArchitecture.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoBotCleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81afd4c6916b3a0133dd63ac58618fd2bf9c8a3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3455210fa411234d78d055db6e6eaa1b084e92bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoBotCleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoBotCleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
