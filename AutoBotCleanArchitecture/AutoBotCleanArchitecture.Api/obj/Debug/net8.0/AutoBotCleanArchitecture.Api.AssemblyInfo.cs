500d6da0e5774701ea66fe1c5448412318ee2a3f
HnguynDevGoon <hnguyndevgoon@gmail.com>
2025-11-17 21:13:07 +0700

    .

--- AutoBotCleanArchitecture/AutoBotCleanArchitecture.Api/obj/Debug/net8.0/AutoBotCleanArchitecture.Api.AssemblyInfo.cs
+++ AutoBotCleanArchitecture/AutoBotCleanArchitecture.Api/obj/Debug/net8.0/AutoBotCleanArchitecture.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoBotCleanArchitecture.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f37674369c47f3a2f9a1ea743e62e85e00a11c3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2db2c862a42c8657b5ec6c6ba1b9081924648873")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoBotCleanArchitecture.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoBotCleanArchitecture.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
